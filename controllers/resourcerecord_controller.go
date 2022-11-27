bc86595e9803b7c0f155e0cae7ee1e93179e90b1
菅原 千晶 <sugawara@pepabo.com>
2022-11-27 16:54:57 +0900

    wip

--- controllers/resourcerecord_controller.go
+++ controllers/resourcerecord_controller.go
@@ -18,7 +18,9 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -37,19 +39,26 @@ type ResourceRecordReconciler struct {
 //+kubebuilder:rbac:groups=dns.ch1aki.github.io,resources=resourcerecords/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=dns.ch1aki.github.io,resources=resourcerecords/finalizers,verbs=update
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the ResourceRecord object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.13.0/pkg/reconcile
 func (r *ResourceRecordReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+        logger := log.FromContext(ctx)
 
-	// TODO(user): your logic here
+        var rr dnsv1alpha1.ResourceRecord
+        err := r.Get(ctx, req.NamespacedName, &rr)
+        if errors.IsNotFound(err) {
+                return ctrl.Result{}, nil
+        }
+        if err != nil {
+                logger.Error(err, "unable to get ResourceRecord", "name", req.NamespacedName)
+                return ctrl.Result{}, err
+        }
+
+        if !rr.ObjectMeta.DeletionTimestamp.IsZero() {
+                return ctrl.Result{}, nil
+        }
+
+        // main logic
+        var owner dnsv1alpha1.ResourceRecordOwner
+        owner = rr.Spec.OwnerRef
 
 	return ctrl.Result{}, nil
 }
